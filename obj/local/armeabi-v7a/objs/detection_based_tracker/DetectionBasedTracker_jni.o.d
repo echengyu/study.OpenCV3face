1d6913967920f4858b34ed8edff8d67d6eb2edd8
Cheng-Yu <e88885@gmail.com>
2016-10-05 12:07:03 +0800

    更新

---
+++ obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o.d
@@ -0,0 +1,121 @@
+obj/local/armeabi-v7a/objs/detection_based_tracker/DetectionBasedTracker_jni.o: \
+ jni/DetectionBasedTracker_jni.cpp jni/DetectionBasedTracker_jni.h \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp \
+ D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h \
+ D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/deque
+
+jni/DetectionBasedTracker_jni.h:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/core.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvdef.h:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/hal/interface.h:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstddef:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/version.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/base.hpp:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/climits:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/algorithm:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.hpp:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstring:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cctype:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/string:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cmath:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/ptr.inl.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/neon_utils.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/traits.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/matx.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/saturate.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/fast_math.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/types.hpp:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cfloat:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/vector:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/mat.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/bufferpool.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/mat.inl.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/persistence.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/operations.hpp:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/cstdio:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/cvstd.inl.hpp:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/complex:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/utility.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/core_c.h:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/types_c.h:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/core/optim.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect/detection_based_tracker.hpp:
+
+D:/Cheng-Yu/Develop-Tool/OpenCV-3.1.0-android-sdk/sdk/native/jni/include/opencv2/objdetect/objdetect_c.h:
+
+D:/Cheng-Yu/Develop-Tool/android-ndk-r12b/build//../sources/cxx-stl/gnu-libstdc++/4.9/include/deque:
